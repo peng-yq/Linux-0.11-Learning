f70aa7ebe9c09e5db80665a896567ed64dfff4f0
PYQ <eilo.pengyq@gmail.com>
2022-11-16 19:57:39 +0800

    2022/11/16

--- fs/inode.c
+++ fs/inode.c
@@ -17,6 +17,8 @@ struct m_inode inode_table[NR_INODE]={{0,},};
 static void read_inode(struct m_inode * inode);
 static void write_inode(struct m_inode * inode);
 
+// 等待指定的 i 节点可用
+// 如果 i 节点已被锁定，则将当前任务置为不可中断的等待状态。直到该 i 节点解锁
 static inline void wait_on_inode(struct m_inode * inode)
 {
 	cli();
@@ -30,32 +32,41 @@ static inline void lock_inode(struct m_inode * inode)
 	cli();
 	while (inode->i_lock)
 		sleep_on(&inode->i_wait);
+	// 锁定节点
 	inode->i_lock=1;
 	sti();
 }
 
+// 直接解锁节点，然后唤醒等待该节点的进程
 static inline void unlock_inode(struct m_inode * inode)
 {
 	inode->i_lock=0;
 	wake_up(&inode->i_wait);
 }
 
+// 释放内存中设备 dev 的所有 i 节点。
+// 扫描内存中的 i 节点表数组，如果是指定设备使用的 i 节点就释放之
 void invalidate_inodes(int dev)
 {
 	int i;
 	struct m_inode * inode;
 
-	inode = 0+inode_table;
+	inode = 0+inode_table;					// 指针指向inode_table数组首项
 	for(i=0 ; i<NR_INODE ; i++,inode++) {
-		wait_on_inode(inode);
+		wait_on_inode(inode);				// 等待节点可用，解锁
+		// 匹配指定设备
 		if (inode->i_dev == dev) {
+			// 节点引用次数不为0则显示出错
 			if (inode->i_count)
 				printk("inode in use on removed disk\n\r");
+			// 释放节点
 			inode->i_dev = inode->i_dirt = 0;
 		}
 	}
 }
 
+// 同步所有 i 节点
+// 同步内存与设备上的所有 i 节点信息
 void sync_inodes(void)
 {
 	int i;
@@ -64,11 +75,18 @@ void sync_inodes(void)
 	inode = 0+inode_table;
 	for(i=0 ; i<NR_INODE ; i++,inode++) {
 		wait_on_inode(inode);
+		// 如果节点已修改且不是管道节点则写入
 		if (inode->i_dirt && !inode->i_pipe)
 			write_inode(inode);
 	}
 }
 
+/*
+ *文件数据块映射到盘块的处理操作。(block 位图处理函数，bmap - block map)
+ *参数：inode – 文件的 i 节点；block – 文件中的数据块号；create - 创建标志
+ *如果创建标志置位，则在对应逻辑块不存在时就申请新磁盘块
+ *返回 block 数据块对应在设备上的逻辑块号（盘块号）
+ */
 static int _bmap(struct m_inode * inode,int block,int create)
 {
 	struct buffer_head * bh;
@@ -76,9 +94,15 @@ static int _bmap(struct m_inode * inode,int block,int create)
 
 	if (block<0)
 		panic("_bmap: block<0");
+	// 如果块号大于直接块数 + 间接块数 + 二次间接块数，超出文件系统表示范围，则死机
+	// 7是i_zone[9]的前7项
 	if (block >= 7+512+512*512)
 		panic("_bmap: block>big");
+	// 直接块
 	if (block<7) {
+		// 如果创建标志置位，并且 i 节点中对应该块的逻辑块（区段）字段为 0，则向相应设备申请一磁盘
+		// 块（逻辑块，区块），并将盘上逻辑块号（盘块号）填入逻辑块字段中。然后设置 i 节点修改时间，
+		// 置 i 节点已修改标志。最后返回逻辑块号。
 		if (create && !inode->i_zone[block])
 			if (inode->i_zone[block]=new_block(inode->i_dev)) {
 				inode->i_ctime=CURRENT_TIME;
@@ -86,6 +110,7 @@ static int _bmap(struct m_inode * inode,int block,int create)
 			}
 		return inode->i_zone[block];
 	}
+	// 如果该块号>=7，并且小于 7+512，则说明是一次间接块。下面对一次间接块进行处理
 	block -= 7;
 	if (block<512) {
 		if (create && !inode->i_zone[7])
@@ -93,19 +118,24 @@ static int _bmap(struct m_inode * inode,int block,int create)
 				inode->i_dirt=1;
 				inode->i_ctime=CURRENT_TIME;
 			}
+		// 若此时 i 节点间接块字段中为 0，表明申请磁盘块失败
 		if (!inode->i_zone[7])
 			return 0;
+		// 读取设备上的一次间接块
 		if (!(bh = bread(inode->i_dev,inode->i_zone[7])))
 			return 0;
+		// 取该间接块上第 block 项中的逻辑块号（盘块号）
 		i = ((unsigned short *) (bh->b_data))[block];
 		if (create && !i)
 			if (i=new_block(inode->i_dev)) {
 				((unsigned short *) (bh->b_data))[block]=i;
 				bh->b_dirt=1;
 			}
+		// 最后释放该间接块，返回磁盘上新申请的对应 block 的逻辑块的块号
 		brelse(bh);
 		return i;
 	}
+	// 二次间接块
 	block -= 512;
 	if (create && !inode->i_zone[8])
 		if (inode->i_zone[8]=new_block(inode->i_dev)) {
@@ -146,7 +176,8 @@ int create_block(struct m_inode * inode, int block)
 {
 	return _bmap(inode,block,1);
 }
-		
+
+// 释放一个 i 节点(回写入设备)	
 void iput(struct m_inode * inode)
 {
 	if (!inode)
@@ -154,6 +185,9 @@ void iput(struct m_inode * inode)
 	wait_on_inode(inode);
 	if (!inode->i_count)
 		panic("iput: trying to free free inode");
+	// 如果是管道 i 节点，则唤醒等待该管道的进程，引用次数减 1，如果还有引用则返回
+	// 否则释放管道占用的内存页面，并复位该节点的引用计数值、已修改标志和管道标志，并返回
+	// 管道节点比较特殊，是内存，也是文件，所以需要释放page(这里的i_size存放物理内存地址)，也要修改inode信息
 	if (inode->i_pipe) {
 		wake_up(&inode->i_wait);
 		if (--inode->i_count)
